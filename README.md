e006314d7f47fc1c361db125164b68a3ed87d3f6
Ao Song <ao.song@outlook.com>
2019-04-26 15:11:01 +0200

    first commit, server app done.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# may
+# May
+
+POC task need be finished before end of May
+
+Requirements:
+==================== 
+1. Single server 
+2. Existing interfaces (op) 
+3. Secure c/s transport (op) 
+4. Non blocking IO (app, c/s)
---
+++ server/src/server_app.erl
@@ -0,0 +1,67 @@
+%%%-------------------------------------------------------------------
+%% @doc server public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(server_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([install/0, install/1]).
+-export([start/2, stop/1]).
+
+-include("server.hrl").
+
+%%====================================================================
+%% API
+%%====================================================================
+
+start(retry, BadTabList) ->
+    case mnesia:wait_for_tables(BadTabList, ?DEFAULT_TIMEOUT*3) of
+        ok -> server_sup:start_link();
+        _TabsNotOK ->
+            {error, "Retried once, database still not prepared."}
+    end;
+start(_StartType, _StartArgs) ->
+    case mnesia:wait_for_tables([service], ?DEFAULT_TIMEOUT) of
+        ok -> server_sup:start_link();
+        {timeout, BadTabList} ->
+            start(retry, BadTabList);
+        {error, Reason} ->
+            {error, Reason}
+    end.
+
+%%--------------------------------------------------------------------
+stop(_State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Install should only be done once. By default only install on single
+%% node.
+%%
+%% @spec install() -> {atomic, ok} | {aborted, Reason}
+%% @end
+%%--------------------------------------------------------------------
+install() ->
+    install([node()]).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Install should only be done once. 
+%%
+%% @spec install(Nodes) -> {atomic, ok} | {aborted, Reason}
+%% @end
+%%--------------------------------------------------------------------
+install(Nodes) ->
+    ok == mnesia:create_schema(Nodes),
+    ok == mnesia:start(),
+    mnesia:create_table(service,
+                        [{disc_copies, Nodes},
+                         {attributes, record_info(fields, service)},
+                         {type, set},
+                         {index, [#service.name]}]).
+%%====================================================================
+%% Internal functions
+%%====================================================================
