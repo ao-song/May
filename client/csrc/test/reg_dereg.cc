88db3591eb2580828a7fc9090cce24fdfaf2c2b2
Ao Song <ao.song@outlook.com>
2019-08-05 14:29:18 +0200

    add test case

--- client/csrc/test/reg_dereg.cc
+++ client/csrc/test/reg_dereg.cc
@@ -1,13 +1,28 @@
 #include <memory>
 #include <cassert>
 #include <thread>
+#include <chrono>
+#include <iostream>
 
 #include "EventHandlerTable.h"
 #include "EnvoyTcp.h"
+#include "Service.h"
+#include "TcpClientOwner.h"
 
 using namespace std;
 
-class EventHandlerTable;
+void event_loop(May::EventHandlerTable* table)
+{
+    while (true)
+    {
+        table->HandleEvents();
+    }
+}
+
+void callback(unsigned char* data)
+{
+    cout << data << endl;
+}
 
 int main()
 {
@@ -15,4 +30,25 @@ int main()
     assert(table->Init() == true);
 
     unique_ptr<May::EnvoyTcp> envoy = unique_ptr<May::EnvoyTcp>(new May::EnvoyTcp(table.get()));
+
+    thread t(event_loop, table.get());
+
+    May::Service service("id_001", "test", "127.0.0.1", 8080, {"a=1", "b=2"});
+    envoy->Register(&service, callback);
+
+    this_thread::sleep_for(chrono::seconds(1));
+
+    string name = "test";
+    envoy->Get(&name);
+
+    string id = "id_001";
+    envoy->Deregister(&id);
+
+    this_thread::sleep_for(chrono::seconds(1));
+
+    envoy->Get(&name);
+
+    t.join();
+
+    return 0;
 }
