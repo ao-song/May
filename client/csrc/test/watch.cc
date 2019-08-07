099889e72628f9fa7058b22b0f37cc6989fa56de
Ao Song <ao.song@outlook.com>
2019-08-07 15:44:13 +0200

    watch test

--- client/csrc/test/watch.cc
+++ client/csrc/test/watch.cc
@@ -1,6 +1,7 @@
 #include <memory>
 #include <cassert>
 #include <thread>
+#include <mutex>
 #include <chrono>
 #include <iostream>
 
@@ -15,12 +16,18 @@ using namespace std;
 
 string watch_id;
 
+mutex mtx;
+
 void watch_cb(unsigned char* data)
 {
+    cout << "Call watch callback with data: " << data << endl;
     json response = json::parse(data);
-    if (response["response"] == "watched" || response["response"] == "watch_updated")
+    if (response["response"].dump() == "watched" || response["response"].dump() == "watch_updated")
     {
-        watch_id = response["watchID"];
+        mtx.lock();
+        watch_id = response["watchID"].dump();        
+        mtx.unlock();
+        
         cout << response["response"] << ", watch ID is: " << watch_id << endl;
     }
     else
@@ -37,24 +44,26 @@ void doer_cb(unsigned char* data)
 
 void watcher(May::EventHandlerTable* table)
 {
+    cout << "Start watcher!" << endl;
     unique_ptr<May::EnvoyTcp> envoy = unique_ptr<May::EnvoyTcp>(new May::EnvoyTcp(table));
-
-    this_thread::sleep_for(chrono::seconds(1));
 
     May::Service service("id_001", "test", "127.0.0.1", 8080, {"a=1", "b=2"});
 
     envoy->Watch(&service, watch_cb);
 
-    this_thread::sleep_for(chrono::seconds(6));
-
+    /*
+    mtx.lock();
     envoy->CancelWatch(watch_id, watch_cb);
+    mtx.unlock();
+    */
 }
 
 void doer(May::EventHandlerTable* table)
 {
+    cout << "Start doer!" << endl;
     unique_ptr<May::EnvoyTcp> envoy = unique_ptr<May::EnvoyTcp>(new May::EnvoyTcp(table));
 
-    this_thread::sleep_for(chrono::seconds(1));
+    this_thread::sleep_for(chrono::seconds(3));
 
     May::Service service("id_001", "test", "127.0.0.1", 8080, {"a=1", "b=2"});
     envoy->Register(&service, doer_cb);
@@ -95,15 +104,19 @@ int main()
     May::EventHandlerTable* table = new May::EventHandlerTable();
     assert(table->Init() == true);
 
+    mtx.lock();
     auto t_watcher = thread(watcher, table);
+    mtx.unlock();
+    mtx.lock();
     auto t_doer = thread(doer, table);
+    mtx.unlock();
 
     int c = 0;
 
-    while (c < 10)
+    while (c < 20)
     {
         table->HandleEvents();
-        this_thread::sleep_for(chrono::microseconds(200));
+        this_thread::sleep_for(chrono::microseconds(1000));
         c += 1;
     }
 
