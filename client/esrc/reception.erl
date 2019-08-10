fcd2489b0f794fe640cec26161aed9a0cdec06ce
Ao Song <ao.song@outlook.com>
2019-08-10 23:38:20 +0200

    add license

--- client/esrc/reception.erl
+++ client/esrc/reception.erl
@@ -8,12 +8,23 @@
 %%%
 %%% @end
 %%% Created : 2019-07-08 09:14:34.467708
-%%%
-%%% Copyright(c) 2019-present, Ao Song <ao.song@outlook.com>
-%%% Distributed under the Apache License, Version 2.0
-%%% (https://www.apache.org/licenses/LICENSE-2.0)
-%%%
 %%%-------------------------------------------------------------------
+
+%%--------------------------------------------------------------------
+%% Copyright 2019 Ao Song <ao.song@outlook.com>
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%--------------------------------------------------------------------
 -module(reception).
 
 -behaviour(gen_server).
--- server/src/server_sup.erl
+++ server/src/server_sup.erl
@@ -4,12 +4,23 @@
 %%%
 %%% @end
 %%% Created : 2019-04-27 11:44:32.215529
-%%%
-%%% Copyright(c) 2019-present, Ao Song <ao.song@outlook.com>
-%%% Distributed under the Apache License, Version 2.0
-%%% (https://www.apache.org/licenses/LICENSE-2.0)
-%%%
 %%%-------------------------------------------------------------------
+
+%%--------------------------------------------------------------------
+%% Copyright 2019 Ao Song <ao.song@outlook.com>
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%--------------------------------------------------------------------
 -module(server_sup).
 
 -behaviour(supervisor).
