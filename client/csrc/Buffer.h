c10bad35d7c6f2a2f28de780a067f88bad715f69
Ao Song <ao.song@outlook.com>
2020-01-27 16:35:43 +0100

    update buffer

--- client/csrc/Buffer.h
+++ client/csrc/Buffer.h
@@ -31,26 +31,25 @@ namespace May
     {        
     public:
         Buffer(size_t size);
+
         Buffer(
             unsigned char* data,
             size_t size);
+
         Buffer(string& str);
+
         Buffer(const Buffer& buff)
         {
             m_size = buff.m_size;
-            m_data = shared_ptr<unsigned char>(new unsigned char[m_size],
-            [](unsigned char* data)
-            {
-                delete[] data;
-            });
-            memcpy(m_data.get(), buff.m_data.get(), m_size);
+            m_data = buff.m_data;
         }
+
         Buffer(Buffer&& buff)
         {
             m_size = buff.m_size;
             m_data = std::move(buff.m_data);
-            buff.m_data.reset();
         }
+
         Buffer& operator=(const Buffer& buff)
         {
             if (this == &buff)
@@ -59,28 +58,18 @@ namespace May
             }
 
             m_size = buff.m_size;
-
-            if (m_data.get() != nullptr)
-            {
-                m_data.reset();
-            }
-            else
-            {
-                m_data = shared_ptr<unsigned char>(new unsigned char[m_size],
-                [](unsigned char* data)
-                {
-                    delete[] data;
-                });
-            }
-            memcpy(m_data.get(), buff.m_data.get(), m_size);
+            m_data = buff.m_data;
             return *this;
         }
+
         ~Buffer();
+
         size_t
         GetSize() const
         {
             return m_size;
         }
+
         unsigned char*
         GetData() const
         {
